cc249512314e718a6d7b6b0dfefa6a89679c2f4b
Ivan Prlic <ip@snowfall.se>
2018-02-13 21:15:49 +0100

    added RooToken to migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Owned = artifacts.require("./owned.sol");
+var RooToken = artifacts.require("./RooToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(Owned);
+  deployer.link(Owned, RooToken);
+  deployer.deploy(RooToken);
 };
