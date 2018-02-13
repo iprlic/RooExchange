6267a80e471b09eee1e237c4ff11b5a1447bcf8a
Ivan Prlic <ip@snowfall.se>
2018-02-13 21:56:09 +0100

    Added RooExchange contract (dummy only)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var RooToken = artifacts.require("./RooToken.sol");
+var RooExchange = artifacts.require("./RooExchange.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(RooToken);
+  deployer.deploy(RooExchange);
 };
